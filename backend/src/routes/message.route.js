3494579f5c877b010ec3258eaedce93a523a2e6e
DipanshuRai <dipanshurai933@gmai.com>
2025-05-28 20:44:59 +0530

    Custom authentication state hook using zustand

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getUsers, getMessages } from "../controllers/message.controller.js";
+import { getUsers, getMessages, sendMessage } from "../controllers/message.controller.js";
 
 const router=express.Router();
 
